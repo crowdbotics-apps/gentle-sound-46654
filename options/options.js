c126865175c4c83d627d3320bdfc42a9f2342c10
Crowdbotics <team@crowdbotics.com>
2024-02-26 23:01:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-sound-46654",
+  url: "https://gentle-sound-46654.botics.co",
+  api: "https://gentle-sound-46654.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
